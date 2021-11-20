eabc2e96dd799574dc346c13f4b70faf26785153
BerkZorbey <tahaberk.yesilalioglu@st.uskudar.edu.tr>
2021-11-20 22:38:08 +0300

    index sayfası oluşturma çabası

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$("box").addEventListener("click",()=>{
+    $("box").css("background-color","black");
+})
+document.getElementsByName("body").addEventListener("click",()=>{
+    $("box").css("background-color","yellow");
+})
