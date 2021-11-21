5fb1e4832351b5de3ffe6383c7926340736c6cd1
BerkZorbey <tahaberk.yesilalioglu@st.uskudar.edu.tr>
2021-11-21 21:14:33 +0300

    index sayfasının jssi geliştirildi

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-$(".berk").click(()=>{
-    $(".berk").css("background-color","black");
-})
+
+
 $(".busra").click(()=>{
-    $(".busra").css("background-color","yellow");
+    $(".busra").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
+    $(".busra h2").css("bottom","50vh");
+    $(".busra h2").css("right","50vw");
+    setTimeout("window.open('main.html','_self')",1000);
+    
+    
 })
+$(".berk").click(()=>{
+    $(".berk").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
+    $(".busra").css("clip-path","polygon(100% 0, 100% 0, 100% 100%, 100% 100%)");
+    $(".berk h2").css("top","50vh");
+    $(".berk h2").css("left","50vw");
+    setTimeout("window.open('berk.html','_self')",1000);
+    
+    
+})
+if($(".busra").clicked==false && $(".berk").clicked==false){
+$(".berk").hover(()=>{
+    $(".berk").css("clip-path","polygon(0 0, 100% 0, 20% 100%, 0% 100%)");
+    $(".busra").css("clip-path","polygon(100% 0, 100% 0, 100% 100%, 20% 100%)");
+})
+$(".berk").mouseleave(()=>{
+    $(".berk").css("clip-path","polygon(0 0, 88% 0, 12% 100%, 0% 100%)");
+    $(".busra").css("clip-path","polygon(88% 0, 100% 0, 100% 100%, 12% 100%)");
+})
+$(".busra").hover(()=>{
+    $(".berk").css("clip-path","polygon(0 0, 80% 0, 0 100%, 0 100%)");
+    $(".busra").css("clip-path","polygon(80% 0, 100% 0, 100% 100%, 0 100%)");
+})
+$(".busra").mouseleave(()=>{
+    $(".berk").css("clip-path","polygon(0 0, 88% 0, 12% 100%, 0% 100%)");
+    $(".busra").css("clip-path","polygon(88% 0, 100% 0, 100% 100%, 12% 100%)");
+})
+}
+
+
