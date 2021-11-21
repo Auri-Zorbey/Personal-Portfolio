cc205acd05c5a5c559272ee7efb5c3f206993197
BerkZorbey <tahaberk.yesilalioglu@st.uskudar.edu.tr>
2021-11-21 21:55:12 +0300

    Page klasörüne html sayfaları konuldu

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ $(".busra").click(()=>{
     $(".busra").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
     $(".busra h2").css("bottom","50vh");
     $(".busra h2").css("right","50vw");
-    setTimeout("window.open('main.html','_self')",500);
+    setTimeout("window.open('../page/main.html','_self')",500);
 })
 $(".berk").click(()=>{
     $(".berk").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
     $(".busra").css("clip-path","polygon(100% 0, 100% 0, 100% 100%, 100% 100%)");
     $(".berk h2").css("top","50vh");
     $(".berk h2").css("left","50vw");
-    setTimeout("window.open('berk.html','_self')",500);
+    setTimeout("window.open('../page/berk.html','_self')",500);
 })
 
 $(".berk").hover(()=>{
