726caac8bf435565fe9451f110d83b07d63b639f
BerkZorbey <tahaberk.yesilalioglu@st.uskudar.edu.tr>
2021-11-21 21:45:10 +0300

    index sayfası bitti

--- js/main.js
+++ js/main.js
@@ -4,20 +4,16 @@ $(".busra").click(()=>{
     $(".busra").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
     $(".busra h2").css("bottom","50vh");
     $(".busra h2").css("right","50vw");
-    setTimeout("window.open('main.html','_self')",1000);
-    
-    
+    setTimeout("window.open('main.html','_self')",500);
 })
 $(".berk").click(()=>{
     $(".berk").css("clip-path","polygon(0 0, 100% 0%, 100% 100%, 0% 100%)");
     $(".busra").css("clip-path","polygon(100% 0, 100% 0, 100% 100%, 100% 100%)");
     $(".berk h2").css("top","50vh");
     $(".berk h2").css("left","50vw");
-    setTimeout("window.open('berk.html','_self')",1000);
-    
-    
+    setTimeout("window.open('berk.html','_self')",500);
 })
-if($(".busra").clicked==false && $(".berk").clicked==false){
+
 $(".berk").hover(()=>{
     $(".berk").css("clip-path","polygon(0 0, 100% 0, 20% 100%, 0% 100%)");
     $(".busra").css("clip-path","polygon(100% 0, 100% 0, 100% 100%, 20% 100%)");
@@ -34,6 +30,6 @@ $(".busra").mouseleave(()=>{
     $(".berk").css("clip-path","polygon(0 0, 88% 0, 12% 100%, 0% 100%)");
     $(".busra").css("clip-path","polygon(88% 0, 100% 0, 100% 100%, 12% 100%)");
 })
-}
+
 
 
